8e54ed490d118aa2a1b9cfca907324f1f20a0d5f
larsan79 <lars.sandstrom@gmail.com>
2024-12-02 19:09:36 +0100

    init

---
+++ Day1/Program.cs
@@ -0,0 +1,49 @@
+﻿var input = await File.ReadAllTextAsync("input.txt");
+var lines = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+// split each line into 2 numbers. store left number in a list and the right number in a list
+var left = new List<int>();
+var right = new List<int>();
+foreach (var line in lines)
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    left.Add(int.Parse(parts[0]));
+    right.Add(int.Parse(parts[1]));
+}
+
+// sort both lists in ascending order
+left.Sort();
+right.Sort();
+
+static void Part1(List<int> left, List<int> right)
+{
+    // iterate through the left list and also the right list and compute the difference between the two numbers and store it in a separate list
+    var diff = new List<int>();
+    for (int i = 0; i < left.Count; i++)
+    {
+        // count the difference between the two numbers. left does not always have to be less than right
+        diff.Add(Math.Abs(left[i] - right[i]));
+    }
+
+    // count the diff list
+    var count = diff.Sum();
+
+    // print the count
+    Console.WriteLine($"Anwser to part 1 is: {count}");
+}
+
+void Part2(List<int> left, List<int> right)
+{
+    // iterate through the left list and find how many occurrences of the same number in the right list then calculate the value times the number of occurrences
+    var count = 0;
+    for (int i = 0; i < left.Count; i++)
+    {
+        var num = left[i];
+        var occurence = right.Count(x => x == num);
+        count += num * occurence;
+    }
+    Console.WriteLine($"Anwser to part 2 is: {count}");
+}
+
+Part1(left, right);
+Part2(left, right);
---
+++ Day2/Program.cs
@@ -0,0 +1,82 @@
+﻿
+var lines = File.ReadAllLines("input.txt");
+
+
+var reports = lines.Select((line, index) =>
+{
+    var levels = line.Split(' ').Select(int.Parse).ToList();
+    return new Report(levels, index);
+}).ToList();
+
+
+var safeReports = reports.Where(r => r.CheckIfSafe_Part1()).ToList();
+Console.WriteLine($"Answer to part 1: {safeReports.Count}");
+
+var safeReports2 = reports.Where(r => r.CheckIfSafe_Part2()).ToList();
+Console.WriteLine($"Answer to part 2: {safeReports2.Count}");
+
+
+Console.WriteLine($"How many reports total: {reports.Count}");
+
+
+
+public class Report(List<int> levels, int index)
+{
+    private readonly int minDiff = 1;
+    private readonly int maxDiff = 3;
+
+    public List<int> Levels => levels;
+
+    void RemoveAt(int i)
+    {
+        levels.RemoveAt(i);
+    }
+
+    private Report Clone()
+    {
+        return new Report(levels.ToList(), index);
+    }
+
+    public bool CheckIfSafe_Part1()
+    {
+        // check if the levels is either all increasing or all decreasing
+        var increasing = levels.Zip(levels.Skip(1), (a, b) => a < b).All(x => x);
+        var decreasing = levels.Zip(levels.Skip(1), (a, b) => a > b).All(x => x);
+
+        if (!increasing && !decreasing)
+        {
+            return false;
+        }
+
+        // any two adjecent levels differ by at least one and at most three
+        var diff = levels.Zip(levels.Skip(1), (a, b) => Math.Abs(a - b)).All(x => x >= minDiff && x <= maxDiff);
+
+        return diff;
+    }
+
+    public bool CheckIfSafe_Part2()
+    {
+        var checkIfSafeWithoutRemovingAnyLevel = CheckIfSafe_Part1();
+        if (checkIfSafeWithoutRemovingAnyLevel)
+        {
+            return true;
+        }
+
+        // check if we can remove any level and still have a safe report
+        for (int i = 0; i < levels.Count; i++)
+        {
+            var newReport = Clone();
+            newReport.RemoveAt(i);
+
+            if (newReport.CheckIfSafe_Part1())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+
+
